c523bddf86156bf1b7ad1a28f5fbc9d785d2a595
MEGA <MEGA@gmail.com>
2025-02-17 18:53:06 +0200

    Add project files.

---
+++ ConsoleApp6/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} na {Age} godini";
+        }
+    }
+}
